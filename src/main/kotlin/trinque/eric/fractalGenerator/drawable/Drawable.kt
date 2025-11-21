fe17f414dfbd1ac972ea9234ee5a83a12c9c3c70
Eric Trinque <TrinqueEric@gmail.com>
2025-11-21 07:03:42 -0500

    Canvas class wrapper added.

---
+++ src/main/kotlin/trinque/eric/fractalGenerator/drawable/Drawable.kt
@@ -0,0 +1,4 @@
+package trinque.eric.fractalGenerator.drawable
+
+interface Drawable {
+}
--- src/main/kotlin/trinque/eric/fractalGenerator/ui/components/Canvas.kt
+++ src/main/kotlin/trinque/eric/fractalGenerator/ui/components/Canvas.kt
@@ -1,6 +1,34 @@
 package trinque.eric.fractalGenerator.ui.components
 
 import javafx.scene.canvas.Canvas
+import javafx.scene.paint.Color
+import trinque.eric.fractalGenerator.drawable.Drawable
 
-class Canvas : Canvas() {
+class Canvas(width: Double, height: Double) : Canvas() {
+    val gc = graphicsContext2D.apply {
+        fill = Color.BLACK
+        stroke = Color.BLACK
+        lineWidth = 2.0
+    }
+
+    fun render(drawable: Drawable) {
+        drawable.draw(gc)
+    }
+
+    fun clear() {
+        gc.clearRect(0.0, 0.0, width, height)
+    }
+
+    fun strokeColor(color: Color) {
+        this.gc.stroke = color
+    }
+
+    fun fillColor(color: Color) {
+        this.gc.fill = color
+    }
+
+    // the default stroke line width is 2.0pt - up from 1.0pt
+    fun lineWidth(width: Double) {
+        this.gc.lineWidth = width
+    }
 }
