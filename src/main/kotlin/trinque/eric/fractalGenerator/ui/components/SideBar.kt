4fe59fcc62e0e7f6fd2aecf5cd4732449442dc45
Eric Trinque <TrinqueEric@gmail.com>
2025-11-21 23:42:07 -0500

    -

--- src/main/kotlin/trinque/eric/fractalGenerator/ui/components/SideBar.kt
+++ src/main/kotlin/trinque/eric/fractalGenerator/ui/components/SideBar.kt
@@ -1,4 +1,44 @@
 package trinque.eric.fractalGenerator.ui.components
 
-class SideBar {
+import javafx.scene.control.Hyperlink
+import javafx.scene.control.Label
+import javafx.scene.layout.HBox
+import javafx.scene.layout.Region
+import javafx.scene.layout.VBox
+
+// TODO
+// SideBar is responsible for composing the data from a network call into a header/body/footer.
+// It makes calls to wikipedia to pull the information about a particular algorithm,
+// displays the information to the user, and provides further links to follow
+
+data class Link(val label: String, val url: String) {}
+
+class SideBar() : Region() {
+    val linkList = mutableListOf<Link>()
+
+    val header: Label = Label()
+    val body = VBox()
+    val footer = HBox()
+
+    fun build(): Region {
+        val sidebar = VBox()
+        sidebar.children.addAll(header, body, footer)
+        return sidebar
+    }
+
+    fun updateLinks() {
+        footer.children.clear()
+        linkList.forEach {
+            val hyperlink = Hyperlink(it.url).apply {
+//                onAction = (it.url)  // url captured in lambda
+            }
+            footer.children.add(hyperlink)
+        }
+    }
+
+    fun addLink(url: String, label: Label) {
+//        linkList.add(Link(url, label))
+    }
 }
+
+
